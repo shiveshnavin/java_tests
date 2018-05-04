4a5d36b462bb5a8ec0121d3a39be757e322b529f
shiveshnavin <shiveshnavin@gmail.com>
2018-05-04 21:28:55 +0530

    Start work on palindrom check

---
+++ PalindromeCheck/src/palindromecheck/PalindromeCheck.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palindromecheck;
+
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author shivesh
+ */
+public class PalindromeCheck {
+
+    
+    private ArrayList<String> getSubStrings(String str)
+    {
+        ArrayList<String> list=new ArrayList<>();
+        
+        for(int i=3;i<str.length()/2;i++)
+        {
+            for(int j=0;j<str.length();j++)
+            {
+                
+            }
+            
+        }
+        
+        return list;
+    }
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan=new Scanner(System.in);
+        String toCheck=scan.nextLine();
+        
+        
+    }
+    
+}
