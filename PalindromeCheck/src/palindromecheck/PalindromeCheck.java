5ae358d76831cff2417d56899501d8cf7ebb4671
shiveshnavin <shiveshnavin@gmail.com>
2018-05-04 22:01:09 +0530

    Done

--- PalindromeCheck/src/palindromecheck/PalindromeCheck.java
+++ PalindromeCheck/src/palindromecheck/PalindromeCheck.java
@@ -14,6 +14,37 @@ import java.util.Scanner;
  */
 public class PalindromeCheck {
 
+    private boolean checkPalindrome(String str)
+    {
+        boolean isPalindrome=true;
+        
+        char x []=str.toCharArray();
+        int len=str.length();
+        for(int i=0;i<len;i++)
+        {
+            isPalindrome=isPalindrome&&(x[i]==x[len-1-i]);
+        }
+        
+        
+        
+        return isPalindrome;
+    }
+    private ArrayList<String> getPalindromes(ArrayList<String> list)
+    {
+        ArrayList<String> listOfPalindromes=new ArrayList<>();
+        
+        for(String str:list)
+        {
+            //System.out.println(str+" Palindrome Check : "+checkPalindrome(str));
+            if(checkPalindrome(str))
+            {
+                listOfPalindromes.add(str);
+            }
+        }
+        
+        
+        return listOfPalindromes;
+    }
     
     private ArrayList<String> getSubStrings(String str)
     {
@@ -26,7 +57,8 @@ public class PalindromeCheck {
                 
                 
                 String subStr=str.substring(index, index+len);
-                System.out.println("Substring : "+subStr);
+                //System.out.println("Substring : "+subStr);
+                list.add(subStr);
                 
                 
             }
@@ -36,19 +68,37 @@ public class PalindromeCheck {
         return list;
     }
     
-    public PalindromeCheck()
-    {
-         Scanner scan=new Scanner(System.in);
-         String toCheck=scan.nextLine();
-        
-        getSubStrings(toCheck);
+    public PalindromeCheck(String toCheck )
+    { 
+         
+       ArrayList<String> listOfPalindromes=getPalindromes(getSubStrings(toCheck));
+       String largestPalindrome=""; 
+       for(String palindrome:listOfPalindromes)
+        {
+           //System.out.println("Found Palindrome : "+palindrome);
+           if(palindrome.length()>largestPalindrome.length())
+           {
+               largestPalindrome=palindrome;
+           }
+          
+        }
+       
+       System.out.println("Largest Palindrome : "+largestPalindrome);
+       
+       
     }
+   
     
     public static void main(String[] args) {
         // TODO code application logic here
        
        System.out.println("Start");
-       PalindromeCheck palCheck=new PalindromeCheck();
+       Scanner scan=new Scanner(System.in);
+       String toCheck=scan.nextLine();
+         
+       
+       PalindromeCheck palCheck=new PalindromeCheck(toCheck);
+      
         
     }
     
